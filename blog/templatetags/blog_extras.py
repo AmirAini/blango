227c5ae1856296e3396faa3e62f00cd0161eb741
Amir Aini <amirhaini@gmail.com>
2023-12-11 12:36:00 +0000

    Finish custom filters

---
+++ blog/templatetags/blog_extras.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+from django.utils.html import format_html
+
+# instantiate the library
+register = template.Library()
+user_model = get_user_model()
+
+
+@register.filter
+def author_details(author: objects,current_user=null : objects):
+    if not isinstance(author, user_model):
+        # return empty string as safe default
+        return ""
+
+    if author.first_name and author.last_name:
+        name = escape(f"{author.first_name} {author.last_name}")
+    else:
+        name = escape(f"{author.username}")
+
+    if author.email:
+        email = escape(author.email)
+        prefix = format_html('<a href="mailto:{}">', author.email)
+        suffix = "</a>"
+    else:
+        prefix = ""
+        suffix = ""
+
+    return mark_safe(f"{prefix}{name}{suffix}")
