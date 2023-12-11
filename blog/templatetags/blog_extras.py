5515f21166be4fb93b4f613d4d93f616f124611b
Amir Aini <amirhaini@gmail.com>
2023-12-11 13:19:53 +0000

    Finish custom filters

--- blog/templatetags/blog_extras.py
+++ blog/templatetags/blog_extras.py
@@ -8,24 +8,26 @@ from django.utils.html import format_html
 register = template.Library()
 user_model = get_user_model()
 
-
 @register.filter
-def author_details(author: objects,current_user=null : objects):
+def author_details(author, current_user):
+    print("hi" + author)
     if not isinstance(author, user_model):
         # return empty string as safe default
         return ""
 
+    if author == current_user:
+        return format_html("<strong>me</strong>")
+
     if author.first_name and author.last_name:
-        name = escape(f"{author.first_name} {author.last_name}")
+        name = f"{author.first_name} {author.last_name}"
     else:
-        name = escape(f"{author.username}")
+        name = f"{author.username}"
 
     if author.email:
-        email = escape(author.email)
         prefix = format_html('<a href="mailto:{}">', author.email)
-        suffix = "</a>"
+        suffix = format_html("</a>")
     else:
         prefix = ""
         suffix = ""
 
-    return mark_safe(f"{prefix}{name}{suffix}")
+    return format_html('{}{}{}', prefix, name, suffix)
