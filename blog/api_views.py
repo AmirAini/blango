995dae6dedebccd93f37bd515d9a7afab882875a
Amir Aini <amirhaini@gmail.com>
2023-12-25 14:03:16 +0000

    Finish Serializers

--- blog/api_views.py
+++ blog/api_views.py
@@ -5,36 +5,25 @@ from django.http import JsonResponse, HttpResponse, HttpResponseNotAllowed
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
 from django.views.decorators.csrf import csrf_exempt
+from blog.api.serializers import PostSerializer
 
-from .models import Post
+from blog.models import Post
 
-def post_to_dict(post):
-  return {
-        "pk": post.pk,
-        "author_id": post.author_id,
-        "created_at": post.created_at,
-        "modified_at": post.modified_at,
-        "published_at": post.published_at,
-        "title": post.title,
-        "slug": post.slug,
-        "summary": post.summary,
-        "content": post.content,
-  }
 
 @csrf_exempt
 def post_list(request):
     if request.method == "GET":
-        posts = Post.objects.all()
-        posts_as_dict = [post_to_dict(p) for p in posts]
-        return JsonResponse({"data": posts_as_dict})
+      posts = Post.objects.all()
+      return JsonResponse({"data": PostSerializer(posts, many=True).data})
     elif request.method == "POST":
-        post_data = json.loads(request.body)
-        post = Post.objects.create(**post_data)
-        return HttpResponse(
+      post_data = json.loads(request.body)
+      serializer = PostSerializer(data=post_data)
+      serializer.is_valid(raise_exception=True)
+      post = serializer.save()
+      return HttpResponse(
             status=HTTPStatus.CREATED,
             headers={"Location": reverse("api_post_detail", args=(post.pk,))},
         )
-
     return HttpResponseNotAllowed(["GET", "POST"])
 
 @csrf_exempt
@@ -42,12 +31,12 @@ def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
 
     if request.method == "GET":
-        return JsonResponse(post_to_dict(post))
+        return JsonResponse(PostSerializer(post).data)
     elif request.method == "PUT":
         post_data = json.loads(request.body)
-        for field, value in post_data.items():
-            setattr(post, field, value)
-        post.save()
+        s = PostSerializer(post,data=post_data, partial=True)
+        if s.is_valid(raise_exception=True):
+          s.save()
         return HttpResponse(status=HTTPStatus.NO_CONTENT)
     elif request.method == "DELETE":
         post.delete()
