19121e7748840131db564fc9e1e6b2895ac9c8a5
Amir Aini <amirhaini@gmail.com>
2023-12-28 09:26:14 +0000

    Finish related fields

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.authtoken import views
+from blog.api.views import UserDetail
 
 from blog.api.views import PostList, PostDetail
 
@@ -10,6 +11,9 @@ urlpatterns = [
     path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
     path('auth/',include("rest_framework.urls")),
     path('token-auth/',views.obtain_auth_token),
+
+    # lookup field is email
+    path("users/<str:email>", UserDetail.as_view(), name="api_user_detail"),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
